ad3543a088613e64acf0cb66df799a08a1307fae
Max <T1tF2f@gmail.com>
2019-11-09 14:56:23 +0200

    321

--- citizenopinion/admin.py
+++ citizenopinion/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import Post
-
+from .models import Question
+from .models import Answer
 admin.site.register(Post)
+admin.site.register(Question)
+admin.site.register(Answer)
