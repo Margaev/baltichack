3169f1c6fb09e4ebbc8272048c676dcb65fdf961
Max <T1tF2f@gmail.com>
2019-11-09 14:34:13 +0200

    Add question and ans

--- citizenopinion/models.py
+++ citizenopinion/models.py
@@ -1,6 +1,8 @@
+# -*- coding:utf-8 -*-
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
+import datetime
 
 class Post(models.Model):
 	title = models.CharField(max_length = 200)
@@ -12,4 +14,31 @@ class Post(models.Model):
 	dis = models.IntegerField()
 	def __str__ (self):
 		 return Post
-
+class Question(models.Model):
+    """Вопрос"""
+    title = models.CharField(max_length=200, verbose_name = "Вопрос")
+    date_published = models.DateTimeField(verbose_name = "Дата публикации", 
+        default = datetime.datetime.now())
+    is_active = models.BooleanField(verbose_name = "Опубликован")
+ 
+    def __str__(self):
+        return self.title
+    
+ 
+    class Meta:
+        verbose_name = 'Вопрос'
+        verbose_name_plural = 'Вопросы'
+  
+ 
+class Answer(models.Model):
+    """Вариант ответа на вопрос"""
+    question_id = models.ForeignKey(Question,on_delete=models.CASCADE)
+    answer = models.CharField(max_length=200, verbose_name = "Ответ")
+    votes = models.IntegerField(verbose_name = "Голосов", default = 0)
+ 
+    def __str__(self):
+        return self.answer
+ 
+    class Meta:
+        verbose_name = 'Ответ'
+        verbose_name_plural = 'Ответы'
