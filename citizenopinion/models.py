b5b6621ef38bcb197f6f49a495b44e5ae734315b
Regaga <danil.petlyakov@gmail.com>
2019-11-09 18:25:25 +0300

    add like

--- citizenopinion/models.py
+++ citizenopinion/models.py
@@ -1,51 +1,65 @@
 # -*- coding:utf-8 -*-
-from django.conf import settings
-from django.db import models
 from django.utils import timezone
+from django.conf import settings
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericRelation
+from django.db import models
 import datetime
+
+class Like(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             related_name='likes',
+                             on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
 
 
 class Post(models.Model):
     title = models.CharField(max_length = 200)
     text = models.TextField()
     create_date = models.DateTimeField(default=timezone.now)
-						#Место откуда фото качаются
-    image = models.ImageField(upload_to = './',null=True)
-    up = models.IntegerField()
-    dis = models.IntegerField()
+						#Место откуда фото качаются cheds
+#    image = models.ImageField(upload_to = '/',height_field = 100, Width_field = 100)
+    likes = GenericRelation(Like)
+    #dislikes = models.IntegerField()
     class Meta:
         verbose_name = 'Статья'
         verbose_name_plural = 'Статьи'
     def __str__ (self):
         return Post
 
+    @property
+    def total_likes(self):
+        return self.likes.count()
 
 class Question(models.Model):
     """Вопрос"""
-    
+
     title = models.CharField(max_length=200, verbose_name = "Вопрос")
-    date_published = models.DateTimeField(verbose_name = "Дата публикации", 
+    date_published = models.DateTimeField(verbose_name = "Дата публикации",
         default = datetime.datetime.now())
     is_active = models.BooleanField(verbose_name = "Опубликован")
- 
+
     def __str__(self):
         return self.title
-    
+
 
     class Meta:
         verbose_name = 'Вопрос'
         verbose_name_plural = 'Вопросы'
-  
- 
+
+
 class Answer(models.Model):
     """Вариант ответа на вопрос"""
     question_id = models.ForeignKey(Question,on_delete=models.CASCADE)
     answer = models.CharField(max_length=200, verbose_name = "Ответ")
     votes = models.IntegerField(verbose_name = "Голосов", default = 0)
- 
+
     def __str__(self):
         return self.answer
- 
+
     class Meta:
         verbose_name = 'Ответ'
         verbose_name_plural = 'Ответы'
