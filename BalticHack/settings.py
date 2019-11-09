25b83964f26692adfd3db94a84528b357a5f6cc6
Margaev <andreymargaev@gmail.com>
2019-11-09 17:42:51 +0300

    Log in / log out and sign up feature

--- BalticHack/settings.py
+++ BalticHack/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'citizenopinion',
+    'citizenopinion.apps.CitizenopinionConfig',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
--- BalticHack/urls.py
+++ BalticHack/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('', include('citizenopinion.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
