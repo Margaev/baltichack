1c9dcac46468eef65f81f5d786467bc22c99dd63
Max <T1tF2f@gmail.com>
2019-11-09 11:42:36 +0200

    add models for news

--- citizenopinion/models.py
+++ citizenopinion/models.py
@@ -1,3 +1,14 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class New(models.Model):
+	title = models.CharField(max_length = 200)
+	text = models.TexField()
+	create_date = models.DateTimeField(default=timezone.now)
+						#Место откуда фото качаются
+	image = models.ImageField(upload_to = '/',height_field = 100, Width_field = 100)
+	up = models.IntegerField()
+	dis = models.IntegerField()
+
+
