530d44f541d90cccf6aac2c54042c4ca0cdf0ac2
Max <T1tF2f@gmail.com>
2019-11-10 08:41:56 +0200

    mmmmm

--- citizenopinion/models.py
+++ citizenopinion/models.py
@@ -14,9 +14,16 @@ class Post(models.Model):
     create_date = models.DateTimeField(default=timezone.now)
     city = models.CharField(max_length = 32)
     image = models.ImageField(upload_to = './', null = True)
-
+    
     def __str__(self):
         return self.title
+
+
+class City(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE) 
+    
+       def __str__(self):
+           return self.post
 
 
 class Poll(models.Model):
@@ -38,6 +45,7 @@ class Choice(models.Model):
 class Votes(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
+
 
     @classmethod
     def create(cls, user, choice):
